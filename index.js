11a2851058500144ae853b3f212b8fe889069a1a
bassamanator <bassam.husain@gmail.com>
2022-01-13 15:01:21 -0500

    First commit.

---
+++ index.js
@@ -0,0 +1,27 @@
+let connectionCounter = 0;
+const PORT = 5011;
+const path = require('path');
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+	next();
+});
+
+app.get('/', (req, res) => {
+	const msg = `Connected[${++connectionCounter}], URL: ${req.url}, Method: ${req.method}`;
+	console.log(msg);
+	res.status(200).json({ message: msg });
+});
+
+app.post('/', (req, res) => {});
+
+app.listen(PORT, () => console.log(`Server 🔥🔥🔥 up on ${PORT}`));
